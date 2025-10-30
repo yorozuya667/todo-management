17c00c47c1822308e97d47da42c534984018b3a1
yorozuya667 <yorozuya667@gmail.com>
2025-10-30 12:01:19 +0500

    lesson 119

--- src/main/java/net/javaguides/todo_management/controller/TodoController.java
+++ src/main/java/net/javaguides/todo_management/controller/TodoController.java
@@ -45,7 +45,7 @@ public class TodoController {
 
     // Build Update Todo REST API
     @PutMapping("{id}")
-    public ResponseEntity<TodoDto> updateTodo(@RequestBody TodoDto todoDto, @PathVariable("id") Long todoId) {
+    public ResponseEntity<TodoDto> updateTodo(@RequestBody  TodoDto todoDto, @PathVariable("id") Long todoId) {
         TodoDto updatedTodo = todoService.updateTodo(todoDto, todoId);
         return ResponseEntity.ok(updatedTodo);
     }
