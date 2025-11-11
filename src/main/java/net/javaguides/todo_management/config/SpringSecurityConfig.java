2079db5bcb25de78ba943a7efdf516353f83ffcb
yorozuya667 <yorozuya667@gmail.com>
2025-11-11 16:35:46 +0500

    lesson 131

--- src/main/java/net/javaguides/todo_management/config/SpringSecurityConfig.java
+++ src/main/java/net/javaguides/todo_management/config/SpringSecurityConfig.java
@@ -2,8 +2,15 @@ package net.javaguides.todo_management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
@@ -11,16 +18,43 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 public class SpringSecurityConfig {
 
     @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)              // ← вместо http.csrf().disable()
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "api/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "api/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "api/**").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.PATCH, "api/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());              // Basic Auth
 
         return http.build();
 
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails Josh = User.builder()
+                .username("Josh")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails Admin = User.builder()
+                .username("Admin")
+                .password(passwordEncoder().encode("Admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(Josh, Admin);
     }
 }
